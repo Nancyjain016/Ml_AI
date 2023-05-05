0f9c84f24eb62f2050596bbf770f394ad8a1c535
Nancyjain016 <nancyjain016@gmail.com>
2023-05-05 11:15:25 +0530

    day13-selfiemachine

--- day12.py
+++ day12.py
@@ -15,12 +15,11 @@ while notCaptured:
             img_gray,
             scaleFactor=1.1,
             minNeighbors=5,
-            minSize=(50, 50)
+            # minSize=(50, 50)
         )
         np.random.seed(50)
         colors = np.random.randint(0, 255, (len(faces), 3)).tolist()
         i = 0
-
         for x, y, w, h in faces:
             face = img[y:y+h, x:x+w, :].copy()
 
@@ -28,12 +27,13 @@ while notCaptured:
                 face,
                 scaleFactor=1.1,
                 minNeighbors=5,
-                minSize=(50, 50)
+                # minSize=(50, 50)
             )
             print(len(smiles))
             if len(smiles) == 1:
                 seq += 1
-                if seq == 10:
+                print(seq)
+                if seq == 2:
                     cv2.imwrite('myselfie.png', img)
                     notCaptured = False
                     break
@@ -48,9 +48,9 @@ while notCaptured:
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
-        else:
-            print('No Frames')
-            break
+    else:
+        print('No Frames')
+        break
     sleep(0.1)
 vid.release()
 cv2.destroyAllWindows()
